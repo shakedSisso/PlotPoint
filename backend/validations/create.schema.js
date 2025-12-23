bc74421c93fda521ad58483db678a0cc4af31572
אורית <oritfrid163@gmail.com>
2025-12-23 11:06:34 +0200

    Update create.schema.js

--- backend/validations/create.schema.js
+++ backend/validations/create.schema.js
@@ -2,7 +2,17 @@ import * as z from "zod";
 
 export const CreateBook = z.object({
   name: z.string().min(2, "Book name must be at least 2 chars"),
-  length: z.int().positive({message: "Length must be greater than 0"}),
+  length: z.int32().positive({message: "Length must be greater than 0"}),
   author: z.string().min(2, "Author name must be at least 2 chars"),
-  category: z.string().min(1, "Select a category"),
+  category: z.int32().positive({message: "Select a category"})
 });
+
+export const CreateCategory = z.object({
+    id: z.int32().positive(),
+    name: z.string().min(2, "Category name must be at least 2 chars")
+})
+
+export const CreateStatus = z.object({
+    id: z.int32().positive(),
+    description: z.string().min(2, "Description must be at least 2 chars")
+})
