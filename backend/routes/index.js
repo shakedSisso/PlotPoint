0e8e56e89abd42c5481ac90bd46ce3bf12487f9f
Shaked Sisso <sisso1011@gmail.com>
2026-01-06 10:44:44 +0200

    Added the shelf routes

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -3,6 +3,7 @@ import authRoutes from "./auth.routes.js";
 import userRoutes from './users.routes.js';
 import buddyReadRoutes from "./buddyRead.routes.js";
 import bookRoutes from './book.routes.js';
+import shelfRoutes from './shelf.routes.js';
 
 //used to create routes easily instead of adding them one by one with app.<delete/get/post>
 const router = Router(); 
@@ -12,6 +13,6 @@ router.use("/auth", authRoutes);
 router.use("/users", userRoutes);
 router.use("/buddyRead", buddyReadRoutes);
 router.use("/books", bookRoutes);
-
+router.use("/shelf", shelfRoutes);
 
 export default router;
