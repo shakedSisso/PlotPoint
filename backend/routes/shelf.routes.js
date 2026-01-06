5664b88d38f9164776f294b4d37ef8a7dcd03c19
Shaked Sisso <sisso1011@gmail.com>
2026-01-06 11:11:56 +0200

    Added the new routes

--- backend/routes/shelf.routes.js
+++ backend/routes/shelf.routes.js
@@ -1,9 +1,12 @@
 import { Router } from "express";
-import { createShelf, getAllShelves } from "../controllers/shelf.controller.js";
+import { createShelf, getAllShelves, addBookToShelf, getBooksFromShelf } from "../controllers/shelf.controller.js";
 
 const router = Router();
 
 router.post("/create", createShelf);
 router.get("/", getAllShelves);
+
+router.post("/add", addBookToShelf);
+router.get("/:shelfName/books", getBooksFromShelf);
 
 export default router;
