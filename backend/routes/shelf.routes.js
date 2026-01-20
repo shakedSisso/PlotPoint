b651e610675906a18b412a897835e4cc353179ec
Shaked Sisso <sisso1011@gmail.com>
2026-01-20 11:23:34 +0200

    Update shelf.routes.js
    Added use of the function `verifyToken`

--- backend/routes/shelf.routes.js
+++ backend/routes/shelf.routes.js
@@ -1,7 +1,9 @@
 import { Router } from "express";
+import { verifyToken } from "../middleware/auth.middleware.js";
 import { createShelf, getAllShelves, addBookToShelf, getBooksFromShelf } from "../controllers/shelf.controller.js";
 
 const router = Router();
+router.use(verifyToken);
 
 router.post("/create", createShelf);
 router.get("/", getAllShelves);
