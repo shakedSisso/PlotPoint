c6c5ad3afab614917ac4c38ec1429e8755fe8048
Shaked Sisso <sisso1011@gmail.com>
2026-01-20 11:24:02 +0200

    Added a logout function

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -57,3 +57,8 @@ export async function login(req, res) {
         res.status(500).json({ success: false, error: "Server error" });
     }
 }
+
+export function logout(req, res) {
+    res.clearCookie("token");
+    res.status(200).json({ success: true, message: "Logged out successfully" });
+}
